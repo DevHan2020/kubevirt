72edae4ba596a3b733b445e452079c62a723ad95
gaopeng <mzzgaopeng@gmail.com>
2021-10-28 17:58:52 +0800

    fix get gw

--- pkg/virt-launcher/virtwrap/network/podinterface.go
+++ pkg/virt-launcher/virtwrap/network/podinterface.go
@@ -25,6 +25,9 @@ import (
 	"encoding/json"
 	"fmt"
 	"io/ioutil"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/apimachinery/pkg/runtime/schema"
+	"k8s.io/client-go/tools/clientcmd"
 	"net"
 	"os"
 	"strconv"
@@ -38,7 +41,7 @@ import (
 
 	"github.com/coreos/go-iptables/iptables"
 	"github.com/vishvananda/netlink"
-
+	"k8s.io/client-go/dynamic"
 	v1 "kubevirt.io/client-go/api/v1"
 	"kubevirt.io/client-go/log"
 	"kubevirt.io/client-go/precond"
@@ -553,7 +556,7 @@ func (b *BridgePodInterface) setInterfaceRoutes() error {
 		return fmt.Errorf("No gateway address found in routes for %s", b.podInterfaceName)
 	}
 	//b.vif.Gateway = routes[0].Gw
-	b.vif.Gateway = getGW(b)
+	b.vif.Gateway = getGW(b.vmi)
 	if len(routes) > 1 {
 		dhcpRoutes := filterPodNetworkRoutes(routes, b.vif)
 		b.vif.Routes = &dhcpRoutes
@@ -561,10 +564,60 @@ func (b *BridgePodInterface) setInterfaceRoutes() error {
 	return nil
 }
 
-func getGW(b *BridgePodInterface) net.IP {
-	peer := b.vif.IP.IP.To4()
-	log.Log.Infof("peer.IP is %v", peer.To4())
-	return net.IPv4(peer[0], peer[1], peer[2], 1)
+type IPPool struct {
+	metav1.TypeMeta   `json:",inline"`
+	metav1.ObjectMeta `json:"metadata,omitempty"`
+
+	Spec   IPPoolSpec   `json:"spec,omitempty"`
+	Status IPPoolStatus `json:"status,omitempty"`
+}
+type IPPoolStatus struct {
+	// INSERT ADDITIONAL STATUS FIELD - define observed state of cluster
+	// Important: Run "make" to regenerate code after modifying this file
+
+	// The number of exclude IPs in the IP pool
+	ExcludeIPCount int `json:"excludeIPCount,omitempty"`
+	// The number of IPs used in the IPPool
+	Using int `json:"using,omitempty"`
+	// The number of IPs available in the IPPool
+	Available int `json:"available,omitempty"`
+}
+type IPPoolSpec struct {
+	// INSERT ADDITIONAL SPEC FIELDS - desired state of cluster
+	// Important: Run "make" to regenerate code after modifying this file
+
+	// The address segment of the IPPool, the cidr method indicates
+	Cidr string `json:"cidr,omitempty"`
+	// The spec.vlan of the IPPool. The valid range is 1-4094.
+	Vlan int `json:"vlan,omitempty"`
+	// Unavailable IP in the IP Pool
+	ExcludeIPs []string `json:"excludeIPs,omitempty"`
+	// Gateway of the IPPool
+	Gateway string `json:"gateway,omitempty"`
+}
+
+func getGW(vm *v1.VirtualMachineInstance) net.IP {
+	ippoolName := vm.Annotations["cmos.ippool"]
+	virtConfig, err := clientcmd.BuildConfigFromFlags("", "")
+	if err != nil {
+		log.Log.Reason(err).Errorf("cannot obtain virt to ipam client: %v\n", err)
+	}
+	dynamicClient := dynamic.NewForConfigOrDie(virtConfig)
+	grv := schema.GroupVersionResource{Group: "ipfixed.cmos.chinamobile", Version: "v1alpha1", Resource: "ippools"}
+	result, err := dynamicClient.Resource(grv).Get(ippoolName, metav1.GetOptions{})
+	if err != nil {
+		log.Log.Reason(err).Errorf("cannot get ippool result: %v\n", err)
+	}
+	data, err := result.MarshalJSON()
+	if err != nil {
+		log.Log.Reason(err).Errorf("cannot get MarshalJSON result: %v\n", err)
+	}
+	var ippool IPPool
+	if err := json.Unmarshal(data, &ippool); err != nil {
+		log.Log.Reason(err).Errorf("cannot get Unmarshal result: %v\n", err)
+	}
+	return net.ParseIP(ippool.Spec.Gateway)
+
 }
 
 func (b *BridgePodInterface) createBridge() error {
